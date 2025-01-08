2a6d3f9099431e4d7cedac8179a07e875c1aeb44
d.khaleghi <d.khaleghi@DPK-37>
2025-01-08 15:01:23 +0330

    CreatingFolders.Part1

---
+++ Orcha/Orcha.Domain/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrchaShop.Domain.Entities
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+        public long CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ Orcha/Orcha.Infrastructure/Persistence/SeedData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrchaShop.Infrastructure.Persistence
+{
+    public static class SeedData
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+           
+        } 
+    }
+}
